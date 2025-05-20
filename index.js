bc063ca2f1689cea73203f821b1f92b2eae26898
Hasin Hafiz <hasinhafiz@Hasins-MacBook-Air.local>
2025-05-20 15:24:57 +0530

    Dockerised node app

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  // Send the response body "Hello, World!"
+  res.end('<h1>Hello, World!</h1>');
+});
+
+// Listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
