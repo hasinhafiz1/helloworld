e0f2b75cae050367a043636bd9730cd267f3bca6
Hasin Hafiz <hasinhafiz@Hasins-MacBook-Air.local>
2025-05-21 10:29:14 +0530

    Push test 2 without keyword

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   // Set the response HTTP header with HTTP status and Content type
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  // Send the response body "Hello, World!"
+  // Send the response body "Hello, World"
   res.end('<h1>Hello, World!</h1>');
 });
 
