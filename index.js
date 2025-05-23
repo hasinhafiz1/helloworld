6dc0ced50abf95a39f0ac8b5bac1ed1759aa9909
hasinhafiz1 <hasin.hafiz@jiitak.com>
2025-05-23 12:15:15 +0530

    docker alb test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Listen on port 3000
-const PORT = 3000;
+const PORT = 80;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
