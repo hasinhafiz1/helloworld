dcc39ad8536efb5d7958bfc402490df60a109564
hasinhafiz1 <hasin.hafiz@jiitak.com>
2025-08-07 19:24:37 +0530

    GCP Deployment Success

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello ,jhkjvjkvhkj:) </h1>');
+  res.end('<h1>Wow this worked...? </h1>');
 });
 
 server.listen(PORT, () => {
